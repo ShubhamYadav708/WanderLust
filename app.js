c69d178623a81082e7e0d54198333cbe92cb63a2
Shubham Yadav <sy3489010@gmail.com>
2026-01-06 12:33:36 +0530

    updated app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000,ys
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     },
 }));
 
